cd308d3f679361bcffd8828377d75f2fe6516c7b
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-03-17 20:49:52 +0100

    finalización con contorno de bordes y arreglos de fonts de cartas

--- Rick_And_Morty/src/app/personajes.service.ts
+++ Rick_And_Morty/src/app/personajes.service.ts
@@ -12,7 +12,7 @@ export class PersonajesService {
   //LA UBICACIÓN DE LA API SE DEBE DE EJECUTAR DESDE EL PROPIO SERVICIO Y NO DE LA CLASE COMPONENTS
   //MÉTODO PARA OBTENER RESPUESTA DE LOS 5 PRIMEROS PERSONAJES, PUEDESN INTERCAMBIARSE POR LOS VALORES DE ID QUE DESEEMOS
   retorna() {
-    return this.http.get("https://rickandmortyapi.com/api/character/1,2,3,4,5");
+    return this.http.get("https://rickandmortyapi.com/api/character/1,801,3,183,5");
   }
 
   //MÉTODO QUE OBTENDRÁ RESPUESTA DE UN ID ENVIADO POR PARÁMETROS
