ecc044dce2485b90252f27ef505547d13538d982
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-03-19 17:20:07 +0100

    revisión del services de personaje y del componente html con los condicionales para administrar valores

--- Rick_And_Morty/src/app/formulario/formulario.component.ts
+++ Rick_And_Morty/src/app/formulario/formulario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnectableObservable } from 'rxjs';
+import { Personajes } from '../models/personajes';
+import { PersonajesService } from '../services/personajes.service';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+  //DEFINICIÓN DEL OBJETO
+  personajes : Personajes = {
+    name : '',
+    status : false,
+    species : '',
+    gender : '',
+    origin : '',
+    location : ''
+
+  }
+
+  submitted = false; //SE DEFINEN EN FALSE YA QUE SOLO AL CLICKAR SE EJECUTARÁ LA ACCIÓN
+
+  constructor(private personajeServices : PersonajesService) { }
 
   ngOnInit(): void {
+  }
+
+  // DEFINIMOS LOS MÉTODOS PARA CADA ACCIÓN LLAMANDO AL SERVICES
+
+  guardaPersonaje() : void {
+    const data = { //EN PRIMERA INSTANCIA LE ADJUMOS LOS VALOES A LAS VARIABLES
+      name : this.personajes.name,
+      species : this.personajes.species,
+      gender : this.personajes.gender,
+      origin : this.personajes.origin,
+      location : this.personajes.location
+    };
+
+    this.personajeServices.agrega(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true; //SOLO CUANDO SE EJECUTE LA FUNCIÓN CAMBIA D EESTADO
+        },
+        error => {
+          console.log(error);
+
+        }
+      );
+  }
+
+  agrega() : void {
+    this.submitted = false;
+    this.personajes = {
+      name : '',
+      status : false,
+      species : '',
+      gender : '',
+      origin : '',
+      location : ''
+    };
   }
 
 }
---
+++ Rick_And_Morty/src/app/models/personajes.ts
@@ -0,0 +1,10 @@
+//CLASE PERSONAJES SOBRE A QUE CONSTRUIREMOS TODO EL CRUD
+export class Personajes {
+  id?: any;
+  name? : string;
+  status? : boolean; //SE PODRÁ CAMBIAR A VIVO O MUERTO
+  species? : string;
+  gender? : string;
+  origin? : string;
+  location? : string;
+}
